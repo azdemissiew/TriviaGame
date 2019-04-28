5d9dfa9f5f0073f8a7cecb16d07b61b6a4422748
azdemissiew <ademssiew@gmail.com>
2019-04-27 23:57:56 -0700

    aded files

---
+++ assets/javascript/question.js
@@ -0,0 +1,32 @@
+var questions =[{
+    "question": "facebook was found in what year?",
+    "option1":2009,
+    "option2":2004,
+    "option3":2005,
+    "option4":2006,
+    "answer": "2"
+}, {
+    "question": "google was found in what year?",
+    "option1":2009,
+    "option2":2004,
+    "option3":2005,
+    "option4":2006,
+    "answer": "3"
+
+},{
+    "question": "amazon was found in what year?",
+    "option1":2009,
+    "option2":2004,
+    "option3":2005,
+    "option4":2006,
+    "answer": "2"
+
+},{"question": "facebook was found in what year?",
+    "option1":2009,
+    "option2":2004,
+    "option3":2005,
+    "option4":2006,
+    "answer": "2"
+
+
+}];
